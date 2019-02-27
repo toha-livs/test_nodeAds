e4f974dce482f08a2a388ee6e51913cb04a5f887
toha-livs <anton.olivson@gmail.com>
2019-02-27 14:41:17 +0200

    delete db

--- test_NodeAds/settings.py
+++ test_NodeAds/settings.py
@@ -79,10 +79,7 @@ WSGI_APPLICATION = 'test_NodeAds.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': env.db()
 }
 
 
